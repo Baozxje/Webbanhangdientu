e384e327f073a0fba0c0110f9d3c6e7693f19dc4
Baozxje <lamgiabao73@gmail.com>
2026-03-08 13:55:42 +0700

    them validation va unique

--- src/main/java/TTDH/Webbanhangdientu/models/User.java
+++ src/main/java/TTDH/Webbanhangdientu/models/User.java
@@ -3,15 +3,27 @@ package TTDH.Webbanhangdientu.models;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.index.Indexed;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 @Document(collection = "users")
 @Data
 public class User {
     @Id
+
     private String id;
+
+    @Indexed(unique = true)
+    @NotBlank(message = "Username không được để trống")
     private String username;
+
     private String password;
+
+    @Indexed(unique = true)
+    @Email(message = "Email không đúng định dạng")
     private String email;
+
     private String role; // "ADMIN" or "USER"
     private String address;
     private String phone;
