654e348c4ade1002cd928374c23fed356ae929d8
Baozxje <lamgiabao73@gmail.com>
2026-03-08 10:12:30 +0700

    Initial Commit

---
+++ src/main/java/TTDH/Webbanhangdientu/WebbanhangdientuApplication.java
@@ -0,0 +1,13 @@
+package TTDH.Webbanhangdientu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebbanhangdientuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebbanhangdientuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TTDH/Webbanhangdientu/WebbanhangdientuApplicationTests.java
@@ -0,0 +1,13 @@
+package TTDH.Webbanhangdientu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebbanhangdientuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
